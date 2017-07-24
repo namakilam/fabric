047ecfedf9aeb1bd10a535b82b8b3e1a1b2be770
Aman <aman.malik@snapdeal.com>
2017-07-24 16:38:23 +0530

    fixed Example

--- examples/chaincode/java/Example/src/main/java/example/Example.java
+++ examples/chaincode/java/Example/src/main/java/example/Example.java
@@ -19,8 +19,11 @@ package example;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperledger.fabric.shim.Chaincode;
 import org.hyperledger.fabric.shim.ChaincodeBase;
 import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.List;
 
 
 /**
@@ -66,41 +69,6 @@ hence no channel call and only effect is line in stdout<br><br>
 public class Example extends ChaincodeBase {
 	 private static Log log = LogFactory.getLog(Example.class);
 
-	@Override
-	public String run(ChaincodeStub stub, String function, String[] args) {
-		log.info("In run, function:"+function);
-		switch (function) {
-		case "put":
-			for (int i = 0; i < args.length; i += 2)
-				stub.putState(args[i], args[i + 1]);
-			break;
-		case "del":
-			for (String arg : args)
-				stub.delState(arg);
-			break;
-		case "hello":
-			System.out.println("hello invoked");
-			log.info("hello invoked");
-			break;
-		}
-		log.error("No matching case for function:"+function);
-		return null;
-	}
-
-	@Override
-	public String query(ChaincodeStub stub, String function, String[] args) {
-		log.info("query");
-		System.out.println("Hello world! function:"+function);
-		log.debug("query:"+args[0]+"="+stub.getState(args[0]));
-		if (stub.getState(args[0])!=null&&!stub.getState(args[0]).isEmpty()){
-			log.trace("returning: Hello world! from "+ stub.getState(args[0]));
-			return "Hello world! from "+ stub.getState(args[0]);
-		}else{
-			log.debug("No value found for key '"+args[0]+"'");
-			return "Hello "+args[0]+"!";
-		}
-	}
-
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello world! starting "+args);
 		log.info("starting");
@@ -108,4 +76,53 @@ public class Example extends ChaincodeBase {
 	}
 
 
+	@Override
+	public Response init(ChaincodeStub chaincodeStub) {
+		chaincodeStub.putState("hello", "world".getBytes());
+		return new Response(Response.Status.SUCCESS, "Initialization O.K", null);
+	}
+
+	@Override
+	public Response invoke(ChaincodeStub chaincodeStub) {
+		switch (chaincodeStub.getFunction()) {
+			case "invoke":
+				return this.Invoke(chaincodeStub);
+			case "query":
+				return this.Query(chaincodeStub);
+		}
+
+		return new Response(Response.Status.SUCCESS, "Incorrect Function Call", null);
+	}
+
+	private Response Query(ChaincodeStub chaincodeStub) {
+		try {
+			List<String> args = chaincodeStub.getParameters();
+
+			if (args.size() != 1) {
+				return newErrorResponse("Incorrect Number of Arguments");
+			}
+
+			return newSuccessResponse(chaincodeStub.getState(args.get(0)));
+		} catch (Exception e) {
+			return newErrorResponse(e);
+		}
+	}
+
+	private Response Invoke(ChaincodeStub chaincodeStub) {
+		try {
+			List<String> args = chaincodeStub.getParameters();
+
+			if (args.size() != 2) {
+				return new Response(Response.Status.SUCCESS, "Incorrect Number Of Arguments", null);
+			}
+
+			String key = args.get(0);
+			String value = args.get(1);
+
+			chaincodeStub.putState(key, value.getBytes());
+			return newSuccessResponse(String.format("Successfully Added Value : %s to Key : %s In the Ledger.", value, key));
+		} catch (Exception e) {
+			return newErrorResponse(e);
+		}
+	}
 }
