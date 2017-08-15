990c2fbcaa9f649db171f8f47073ab6c80805020
Aman Malik <aman.malik@snapdeal.com>
2017-08-15 12:25:43 +0530

    e2e-example-network-setup

--- examples/e2e_cli/generateArtifacts.sh
+++ examples/e2e_cli/generateArtifacts.sh
@@ -42,7 +42,7 @@ function replacePrivateKey () {
 
 ## Generates Org certs using cryptogen tool
 function generateCerts (){
-	CRYPTOGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/cryptogen
+	CRYPTOGEN=/Users/namakilam/workspace/go/src/github.com/hyperledger/fabric/bin/cryptogen
 
 	if [ -f "$CRYPTOGEN" ]; then
             echo "Using cryptogen -> $CRYPTOGEN"
@@ -62,7 +62,7 @@ function generateCerts (){
 ## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
 function generateChannelArtifacts() {
 
-	CONFIGTXGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/configtxgen
+	CONFIGTXGEN=/Users/namakilam/workspace/go/src/github.com/hyperledger/fabric/bin/configtxgen
 	if [ -f "$CONFIGTXGEN" ]; then
             echo "Using configtxgen -> $CONFIGTXGEN"
 	else
